bbabad3f58460d8aa70ff81463c17fc6de13ec9c
mert <mert@auction-experts.com>
2023-08-21 22:18:50 +0200

    Product & ProductFeatures & ProductRepository were added

---
+++ ElastichSearch.API/Models/EColor.cs
@@ -0,0 +1,7 @@
+﻿namespace ElastichSearch.API.Models
+{
+    public enum EColor
+    {
+        Red = 1, Green = 2, Blue = 3,
+    }
+}
---
+++ ElastichSearch.API/Repositories/ProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using ElastichSearch.API.Models;
+using Nest;
+
+namespace ElastichSearch.API.Repositories
+{
+    public class ProductRepository
+    {
+        private readonly ElasticClient _elastichClient;
+
+
+        public ProductRepository(ElasticClient elastichClient)
+        {
+            _elastichClient = elastichClient;
+        }
+
+        public async Task<Product?> SaveAsync(Product newProduct)
+        {
+            newProduct.Created = DateTime.Now;
+
+            var response = await _elastichClient.IndexAsync(newProduct, x => x.Index("products"));
+
+            if (!response.IsValid) return null;
+
+            newProduct.Id = response.Id;
+
+            return newProduct;
+
+        }
+    }
+}
